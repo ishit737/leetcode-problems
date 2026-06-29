f78a51122a25d2fe590791b935837512ea9425b1
ishit737 <ishit737@gmail.com>
2026-06-29 19:16:54 +0530

    Time: 39 ms (67.54%), Space: 12.6 MB (9.59%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def shuffle(self, nums, n):
+        """
+        :type nums: List[int]
+        :type n: int
+        :rtype: List[int]
+        """
+        a=0
+        b=[]
+        for i in range(n):
+            b.append(nums[i])  
+            a=i+(len(nums)//2) 
+            b.append(nums[a])
+        return b    
