366b89ae30192bc0d30a788824943cf20984fa4f
ishit737 <ishit737@gmail.com>
2026-06-29 19:18:34 +0530

    Time: 36 ms (82.92%), Space: 12.3 MB (98.35%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,14 +1,9 @@
 class Solution(object):
     def shuffle(self, nums, n):
-        """
-        :type nums: List[int]
-        :type n: int
-        :rtype: List[int]
-        """
-     
-        b=[]
+        b = []
+
         for i in range(n):
-            b.append(nums[i])  
-            a=i+(len(nums)//2) 
-            b.append(nums[a])
-        return b    
+            b.append(nums[i])
+            b.append(nums[i + n])
+
+        return b  
