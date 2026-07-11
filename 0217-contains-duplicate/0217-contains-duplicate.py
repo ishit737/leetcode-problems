2ed7352da897b2622de7b55a1a131b4e13ae710f
ishit737 <ishit737@gmail.com>
2026-07-11 23:57:15 +0530

    Update 0217-contains-duplicate.py

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -8,4 +8,10 @@ class Solution(object):
    
         return len(nums) != len(set(nums))
 
+    ##brute force,has time complexity O(N^2).other method can be using set(nums) to make it faster
+    for i in nums:
+        if nums.count(i)>1:
+            return True
+     return False       
+
      
