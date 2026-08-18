676ac951eb04516ead36a77e8762ff022a3b6772
ishit737 <ishit737@gmail.com>
2026-08-18 23:27:04 +0530

    Time: 17 ms (24.44%), Space: 14.1 MB (92.15%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -4,10 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        a=[]
-        for i in nums:
-            a.append(i**2)
-        a.sort()
-        return a
+        left = 0
+        right = len(nums) - 1
+        res = [0] * len(nums)
+        pos = len(nums) - 1
 
-        
+        while left <= right:
+            if nums[right]**2 >= nums[left]**2:
+                res[pos] = nums[right]**2
+                right = right - 1
+            else:
+                res[pos] = nums[left]**2
+                left = left + 1
+            
+            pos = pos - 1 
+
+        return res  
+            
